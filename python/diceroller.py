908dc038a673900410289715a27b49bd0b672f24
Kaamkiya <138614660+Kaamkiya@users.noreply.github.com>
2023-10-25 17:21:38 -0400

    add comments and docstring to diceroller
    add comments, docstrings, error catching

--- python/diceroller.py
+++ python/diceroller.py
@@ -1,14 +1,45 @@
+"""
+Dice Roller
+
+Roll an amount of dice with an amount of sides chosen by the user.
+"""
+
 import random
 
-print('How many dice would you like to roll?')
-amount_of_dice = int(input('number> '))
+def main():
+    print('How many dice would you like to roll?')
+    
+    try:
+        amount_of_dice = int(input('> ')) # try to convert the answer to an int
+    except:
+        print('Not a valid number.') # if it doesn't work, return 'y'
+        return 'y' # returning this causes the prgram to run again
+    
+    print('How many sides should the dice have?')
+    
+    try:
+        amount_of_sides = int(input('> ')) # same as above, pretty much
+    except:
+        print('Not a valid number.')
+        return 'y'
+    
+    total = 0
+    
+    for i in range(amount_of_dice):
+        # add a random number between 1 and the number of sides to the total
+        total += random.randint(1, amount_of_sides)
+    
+    print('Total:', total)
+    
+    again = ''
+    while again not in ['y', 'n']:
+        print('Would you like to go again? (Y/N)')
+        again = input('> ').lower()
 
-print('How many sides should each die have?')
-amount_of_sides = int(input('number> '))
+    return again
 
-total = 0
+while True:
+    if main() == 'n': # if the user does not want to play again,
+        break # stop and exit
 
-for i in range(amount_of_dice):
-	total += random.randint(1, amount_of_sides)
-
-print('Total: %s' %total)
+print('Thanks for playing!')
